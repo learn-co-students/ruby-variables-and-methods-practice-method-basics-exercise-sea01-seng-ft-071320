9370130726f57a39016d900ca506bc88b8e0c3cc
github username <rss3509@gmail.com>
2020-07-20 09:48:04 -0700

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
-end
-
-def assign_variable(value)
+    num1/num2
+  end
   
-end
-
-def argue
+  def assign_variable(value)
+    name = value 
+  end
   
-end
-
-def greeting
-
-end
-
-def return_a_value
-
-end
-
-def last_evaluated_value
-
-end
-
-def pizza_party
-
-end
+  def argue(argument)
+    argument
+  end
+  
+  def greeting(greeting, name)
+    puts "#{greeting}, #{name}"
+  end
+  
+  def return_a_value
+    "Nice"
+  end
+  
+  def last_evaluated_value
+    "beginner"
+    "expert"
+  end
+  
+  def pizza_party(pie="cheese")
+    pie
+  end
